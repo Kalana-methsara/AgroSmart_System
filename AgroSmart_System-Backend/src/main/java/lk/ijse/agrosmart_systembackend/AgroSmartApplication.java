5c5c8ef5826405044f00af1ba9ccff5128ae3192
Kalana-methsara <kalanamethsara53@gmail.com>
2026-03-04 19:46:21 +0530

    first commit

---
+++ AgroSmart_System-Backend/src/main/java/lk/ijse/agrosmart_systembackend/AgroSmartApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.agrosmart_systembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroSmartApplication.class, args);
+    }
+
+}
---
+++ AgroSmart_System-Backend/src/test/java/lk/ijse/agrosmart_systembackend/AgroSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.agrosmart_systembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
