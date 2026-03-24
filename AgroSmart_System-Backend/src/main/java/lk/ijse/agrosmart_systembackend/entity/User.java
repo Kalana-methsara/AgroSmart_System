6eea3c9bd755c6d4e85ab0d5f0b3d9e9fcbd3a1a
Kalana-methsara <kalanamethsara53@gmail.com>
2026-03-24 09:03:31 +0530

    dashbord modified

--- AgroSmart_System-Backend/src/main/java/lk/ijse/agrosmart_systembackend/entity/User.java
+++ AgroSmart_System-Backend/src/main/java/lk/ijse/agrosmart_systembackend/entity/User.java
@@ -5,13 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
