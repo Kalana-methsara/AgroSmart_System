326f7e57915c87775ad0758313a3e818a2c0c175
Kalana-methsara <kalanamethsara53@gmail.com>
2026-03-30 10:29:05 +0530

    complete cultivation front

--- AgroSmart_System-Backend/src/main/java/lk/ijse/agrosmart_systembackend/repository/CropTaskTemplateRepository.java
+++ AgroSmart_System-Backend/src/main/java/lk/ijse/agrosmart_systembackend/repository/CropTaskTemplateRepository.java
@@ -4,9 +4,11 @@ import lk.ijse.agrosmart_systembackend.dto.CropTaskTemplateDTO;
 import lk.ijse.agrosmart_systembackend.entity.Crop;
 import lk.ijse.agrosmart_systembackend.entity.CropTaskTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CropTaskTemplateRepository extends JpaRepository<CropTaskTemplate,String> {
     List<CropTaskTemplate> findByCropId(String cropId);
 }
--- AgroSmart_System-Backend/src/main/java/lk/ijse/agrosmart_systembackend/repository/PlantingTaskRepository.java
+++ AgroSmart_System-Backend/src/main/java/lk/ijse/agrosmart_systembackend/repository/PlantingTaskRepository.java
@@ -2,6 +2,8 @@ package lk.ijse.agrosmart_systembackend.repository;
 
 import lk.ijse.agrosmart_systembackend.entity.PlantingTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlantingTaskRepository extends JpaRepository<PlantingTask, String> {
 }
